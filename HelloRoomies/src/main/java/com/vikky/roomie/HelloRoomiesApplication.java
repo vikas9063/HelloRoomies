a926aecc5061d2a81e66d2937b54902b97ae7124
Sreenivas Bandaru <Sreenivas Bandaru@10.0.2.15>
2022-06-13 14:57:19 +0530

    ok first commit.

---
+++ HelloRoomies/src/main/java/com/vikky/roomie/HelloRoomiesApplication.java
@@ -0,0 +1,13 @@
+package com.vikky.roomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRoomiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRoomiesApplication.class, args);
+	}
+
+}
---
+++ HelloRoomies/src/test/java/com/vikky/roomie/HelloRoomiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikky.roomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRoomiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
